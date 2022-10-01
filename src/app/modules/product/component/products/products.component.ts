9be9cdb9e54f5208b9ed156b21e53aa09a7875bc
Tarek ahmed <tarek.ahmed@intouch-technology.com>
2022-10-01 23:51:12 +0200

    Fixed ui issues and added read me

--- src/app/modules/product/component/products/products.component.ts
+++ src/app/modules/product/component/products/products.component.ts
@@ -13,29 +13,32 @@ import { EditQuantityComponent } from '../../_popups/edit-quantity/edit-quantity
 export class ProductsComponent implements OnInit {
   products: products[] = [];
   selectedProducts: products[] = [];
-  @ViewChild('newOrder', {static: false}) newOrder!: AddNewOrderComponent
-  @ViewChild('editQuantity', {static: false}) editQuantity!: EditQuantityComponent
+  count: number = 0;
+  @ViewChild('newOrder', { static: false }) newOrder!: AddNewOrderComponent
+  @ViewChild('editQuantity', { static: false }) editQuantity!: EditQuantityComponent
 
-  
+
 
   constructor(private route: ActivatedRoute) { }
-//Fetch data from resolver
+  //Fetch data from resolver
   ngOnInit(): void {
-    this.products =this.route.snapshot.data['products'];
+    this.products = this.route.snapshot.data['products'];
   }
 
-//Add products to the cart
+  //Add products to the cart
   selectProducts(products: products) {
     this.selectedProducts.push(products)
+    this.count = +this.count
+
   }
-//Open cart popup
+  //Open cart popup
   openModal() {
     this.newOrder.open();
     this.newOrder.selecedProduct(this.selectedProducts)
   }
-  
-//Open edit quantity popup
-  openEditQuantity(products: products){
+
+  //Open edit quantity popup
+  openEditQuantity(products: products) {
     this.editQuantity.open();
     this.editQuantity.editProduct(products)
   }
---
+++ src/assets/readme.md
@@ -0,0 +1,14 @@
+How to use Products Page ?
+products page contain list of products every card contain product details such as price and quantities.
+
+products that have less than 10 Available Pieces will be highlighted in red you can edit Available Pieces
+by click the edit icon
+
+When click on add to cart the product you select will be in the shopping cart and you can choose multiple product.
+
+You can Checkout and select payment in shopping cart click on checkout.
+__________________________________________________________________________________________________________
+
+Orders page contain list of all orders with payment method and if you click on the info icon you will open selected order details.
+
+Order details contain the user information and order quantities.
